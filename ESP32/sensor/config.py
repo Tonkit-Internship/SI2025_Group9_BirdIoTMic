af854c919b14810433a162ec628dab870aeeb441
Meen832548 <k0925357882@gmail.com>
2026-03-11 13:26:58 +0700

    change version bird net from 2.1.1 to 2.4.0

--- ESP32/sensor/config.py
+++
@@ -1,6 +0,0 @@
-WIFI_SSID = "Meen88888"#"412aisfibre_2.4G""Meen8888"
-WIFI_PASS = "08032548"#"Ponmgolf""08032548"
-API_URL = "https://aqxls4je9e.execute-api.us-east-1.amazonaws.com/upload_metadata_and_Sound"
-BOARD_ID = "ESP32-02" 
-LOCATION = "Thammasat_University_Rangsit"
-
